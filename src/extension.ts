cf046823a0df78ce62616153174098d9cc50a5c5
Yogi Astawan <yogi.astawan@gmail.com>
2020-07-27 16:55:20 +0800

    change output project structure

--- src/extension.ts
+++ src/extension.ts
@@ -2,62 +2,66 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as menuContentxml from "./menuxml";
+import * as windowContentxml from "./windowxml";
+import * as mainC from "./mainc";
+import * as makefileCreator from "./makefile";
+import * as gResource from "./res";
 
 //create name input
-function createNameInput(){
-	var option: vscode.InputBoxOptions={
-		ignoreFocusOut:false,
-		placeHolder:"Input here your project name...",
-		prompt:"Type here your project name."
+function createNameInput() {
+	var option: vscode.InputBoxOptions = {
+		ignoreFocusOut: false,
+		placeHolder: "Input here your project name...",
+		prompt: "Type here your project name."
 	};
 	return vscode.window.showInputBox(option);
 }
 
 //create folder structure
-function createFolderStructureNewProject(nameProject:string){
-	var currentDirectory=vscode.workspace.rootPath;
+function createFolderStructureNewProject(nameProject: string) {
+	var currentDirectory = vscode.workspace.rootPath;
 	console.log(currentDirectory);
-	if(currentDirectory===null){
+	if (currentDirectory === null) {
 		vscode.window.showErrorMessage("Cannot Create Project. Please open folder first.");
 		return false;
-	}else{
-		var name=nameProject.replace(' ','');
-		fs.mkdirSync(currentDirectory+"/"+name+"/src",{recursive: true});
-		fs.mkdirSync(currentDirectory+"/"+name+"/res/ui",{recursive:true});
-		fs.mkdirSync(currentDirectory+"/"+name+"/res/menu",{recursive:true});
-		fs.mkdirSync(currentDirectory+"/"+name+"/res/drawable",{recursive:true});
-		fs.mkdirSync(currentDirectory+"/"+name+"/res/drawable-vector",{recursive:true});
+	} else {
+		var name = nameProject.replace(/ /g , "");
+		fs.mkdirSync(currentDirectory + "/" + name + "/src", { recursive: true });
+		fs.mkdirSync(currentDirectory + "/" + name + "/res/ui", { recursive: true });
+		fs.mkdirSync(currentDirectory + "/" + name + "/res/menu", { recursive: true });
+		fs.mkdirSync(currentDirectory + "/" + name + "/res/drawable", { recursive: true });
+		fs.mkdirSync(currentDirectory + "/" + name + "/res/drawable-vector", { recursive: true });
 		return true;
-	}	
+	}
 }
 
 //create file project
-function createFileNewProject(nameProject:string) {
-	var currentDirectory=vscode.workspace.rootPath;
-	var name=nameProject.replace(' ','_').toLowerCase();
-	var nameDir=nameProject.replace(' ','');
-	var content=createCContent(nameProject);
-	fs.writeFileSync(currentDirectory+"/"+nameDir+"/src/"+name+".c",content);
-	content=createWindowUIContent(nameProject);
-	fs.writeFileSync(currentDirectory+"/"+nameDir+"/res/ui/"+name+".ui",content);
-}
-
-//create c content
-function createCContent(nameProject:string){
-	var content="This is C";
-	return content;
-}
-
-//create window xml
-function createWindowUIContent(nameProject:string){
-	var content="This is ui xml";
-	return content;
-}
-
-//create application menu bar xml
-function createMenuUI(nameProject:string){
-	var content="this is menu xml";
-	return content;
+function createFileNewProjectWithUI(nameProject: string) {
+	var currentDirectory = vscode.workspace.rootPath;
+	var name = nameProject.replace(/ /g, '_').toLowerCase();
+	var nameDir = nameProject.replace(/ /g, '');
+	//create main c file
+	var content = mainC.mainWithUI(nameProject);
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/src/main_" + name + ".c", content);
+	//create ui window file
+	content = windowContentxml.createWindowContent(nameProject);
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/res/ui/" + name + ".ui", content);
+	//create menu.ui file
+	content = menuContentxml.menuxmlcontent();
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/res/menu/menu.ui", content);
+	//create resource file
+	content=gResource.contentRes(nameProject);
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/res/res_"+nameDir+".gresource.xml", content);
+	//create Makefile project	
+	content=makefileCreator.contentMakeFileProject(nameProject);
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/Makefile", content);
+	//create Makefile src project	
+	content=makefileCreator.contentMakeFileSrc(nameProject);
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/src/source.mk", content);
+	//create Makefile res project	
+	content=makefileCreator.contentMakeFileRes(nameProject);
+	fs.writeFileSync(currentDirectory + "/" + nameDir + "/res/resource.mk", content);
 }
 
 // this method is called when your extension is activated
@@ -76,26 +80,26 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Gisa Gtk+3 Project!');
-		let name=createNameInput().then(async function(res){
-			if(!res){
+		let name = createNameInput().then(async function (res) {
+			if (!res) {
 				vscode.window.showErrorMessage("Cannot create project");
-			}else{
-				console.log("input: "+res);
+			} else {
+				console.log("input: " + res);
 				//create folder
-				var folderSuccess=createFolderStructureNewProject(res);
+				var folderSuccess = createFolderStructureNewProject(res);
 				if (!folderSuccess) {
 					vscode.window.showErrorMessage("Cannot create project: Create base folder failed.");
-				}else{
+				} else {
 					//create file
-					createFileNewProject(res);
+					createFileNewProjectWithUI(res);
 				}
 			}
 		});
-						
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
