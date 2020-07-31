8ccfd6a7d0c90ba00711ee8c82b87646fb231594
Yogi Astawan <yogi.astawan@gmail.com>
2020-08-01 05:05:36 +0800

    add create custom widget command

--- src/extension.ts
+++ src/extension.ts
@@ -70,7 +70,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gisa-gtk-3-project" is now active!');
+	//console.log('Congratulations, your extension "gisa-gtk-3-project" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -95,6 +95,10 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			}
 		});
+
+	});
+
+	let disposable2=vscode.commands.registerCommand('gisa-gtk-3-project.createcustomwidget',()=>{
 
 	});
 
