cd131be6a011ab8b0130df4f87c1795ec4075ec6
Yogi Astawan <yogi.astawan@gmail.com>
2020-08-02 11:59:05 +0800

    add make file content for pure c project

--- src/makefile.ts
+++ src/makefile.ts
@@ -16,8 +16,7 @@ clean:
 	@cd res; make -f resource.mk clean
 	@cd src; make -f source.mk clean
 	@rm -rf build-debug
-	@rm -rf build-release
-`;
+	@rm -rf build-release`;
     return con;
 }
 
@@ -128,5 +127,24 @@ $(DBGBUILDDIR)res/%.c: %.gresource.xml
 clean:
 	rm -f $(RELOBJ) $(RELCFILE) $(DBGOBJ) $(DBGCFILE)
 	rm -rf $(RELBUILDDIR)res $(DBGBUILDDIR)res`;
+    return con;
+}
+
+export function contentMakeFileProjetPureC(nameApp: string) {
+    var con: string;
+    con = `release:
+	@echo -e "=>\\e[94mCreate \\"\\e[4m${nameApp}\\e[24m\\" executable to release\\e[0m"
+	@mkdir -p build-release
+	@cd src; make -f source.mk release
+
+debug:
+	@mkdir -p build-debug
+	@echo -e "=>\\e[94mCreate \\"\\e[4m${nameApp}\\e[24m\\" executable to debug\\e[0m"
+	@cd src; make -f source.mk debug
+
+clean:	
+	@cd src; make -f source.mk clean
+	@rm -rf build-debug
+	@rm -rf build-release`;
     return con;
 }
