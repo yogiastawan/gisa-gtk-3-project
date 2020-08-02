469670cc5eca04cfaf8db9ca2c83dfa4ac2c1de6
Yogi Astawan <yogi.astawan@gmail.com>
2020-08-02 11:59:47 +0800

    add create new project without xml UI

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,6 @@ import * as mainC from "./mainc";
 import * as makefileCreator from "./makefile";
 import * as gResource from "./res";
 import * as customWidgetContent from "./customwidget";
-import { fileURLToPath } from 'url';
 
 //create name input
 function createNameInput() {
@@ -73,6 +72,29 @@ function createFileNewProjectWithUI(nameProject: string) {
 	}
 }
 
+function createFileNewProject(nameProject: string) {
+	var workspaces: ReadonlyArray<vscode.WorkspaceFolder> | undefined;
+	workspaces = vscode.workspace.workspaceFolders;
+	if (workspaces) {
+		var name = nameProject.replace(/ /g, '_').toLowerCase();
+		var nameDir = nameProject.replace(/ /g, '');
+		//create main c file
+		var content = mainC.mainWithUI(nameProject);
+		fs.writeFileSync(`${workspaces[0].uri.path}/${nameDir}/src/main_${name}.c`, content);		
+		//create Makefile project	
+		content = makefileCreator.contentMakeFileProjetPureC(nameProject);
+		fs.writeFileSync(`${workspaces[0].uri.path}/${nameDir}/Makefile`, content);
+		//create Makefile src project	
+		content = makefileCreator.contentMakeFileSrc(nameProject);
+		fs.writeFileSync(`${workspaces[0].uri.path}/${nameDir}/src/source.mk`, content);
+		vscode.commands.executeCommand('vscode.openFolder',vscode.Uri.file(`${workspaces[0].uri.path}/${nameDir}`));
+		
+	} else {
+		vscode.window.showErrorMessage("Cannot Create File to Project");
+	}
+}
+
+
 //new custom_widget
 function createNameWidget() {
 	var option: vscode.InputBoxOptions = {
@@ -122,7 +144,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gisa-gtk-3-project.createproject', () => {
+	let disposable = vscode.commands.registerCommand('gisa-gtk-3-project.createprojectwithUI', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
@@ -139,6 +161,29 @@ export function activate(context: vscode.ExtensionContext) {
 				} else {
 					//create file
 					createFileNewProjectWithUI(res);					
+				}
+			}
+		});
+
+	});
+
+	let pureCproj = vscode.commands.registerCommand('gisa-gtk-3-project.createproject', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World from Gisa Gtk+3 Project!');
+		let name = createNameInput().then(async function (res) {
+			if (!res) {
+				vscode.window.showErrorMessage("Cannot create project");
+			} else {
+				// console.log("input: " + res);
+				//create folder
+				var folderSuccess = createFolderStructureNewProject(res);
+				if (!folderSuccess) {
+					vscode.window.showErrorMessage("Cannot create project: Create base folder failed.");
+				} else {
+					//create file
+					createFileNewProject(res);					
 				}
 			}
 		});
