fea7cb0dd68243f9d71d71266f20145239f13a54
Yogi Astawan <yogi.astawan@gmail.com>
2021-05-02 01:56:26 +0800

    add -lm and -ldl flags to gcc

--- src/makefile.ts
+++ src/makefile.ts
@@ -26,6 +26,7 @@ export function contentMakeFileSrc(nameApp: string) {
     var con: string;
     con = `CC= gcc
 CFLAGS= -Wall -Werror -Wextra -v -std=c99
+CFLAGSEND= -lm -ldl
 DBGCFLAGS= -g
 
 GTKFLAGS=\`pkg-config --cflags --libs gtk+-3.0\`
@@ -45,28 +46,28 @@ release: $(RELEXEC)
 debug: $(DBGEXEC)
 $(RELEXEC): $(RELOBJS) $(RELOBJRES)
 	@echo -e "=>\\e[94mCreate \\"\\e[4m$(@F)\\e[24m\\" executable to release\\e[0m"
-	$(CC) $(CFLAGS) -o $@ $^ $(GTKFLAGS)
+	$(CC) $(CFLAGS) -o $@ $^ $(CFLAGSEND) $(GTKFLAGS)
 	@echo -e "=>\\e[92mCreating \\"\\e[4m$(@F)\\e[24m\\" into \\"\\e[4m$(@D)\\e[24m\\" Success\\e[0m"
 
 $(RELBUILDDIR)src/%.o: %.c
 	@echo -e "=>\\e[94mPreparing \\"\\e[4m$<\\e[24m\\" to release\\e[0m"
 
 	@mkdir -p $(RELBUILDDIR)/src/$(<D)
-	$(CC)  $(CFLAGS) -c $< -o $@ $(GTKFLAGS)
+	$(CC)  $(CFLAGS) -c $< -o $@ $(CFLAGSEND) $(GTKFLAGS)
 	@echo -e "=>\\e[92mBuilding \\"\\e[4m$(@F)\\e[24m\\" into \\"\\e[4m$(@D)\\e[24m\\" Success\\e[0m"
 
 ############################# FOR DEBUG #######################
 
 $(DBGEXEC): $(DBGOBJS) $(DBGOBJRES)
 	@echo -e "=>\\e[94mCreate \\"\\e[4m$(@F)\\e[24m\\" executable to debug\\e[0m"
-	$(CC) $(CFLAGS) $(DBGCFLAGS) -o $@ $^ $(GTKFLAGS)
+	$(CC) $(CFLAGS) $(DBGCFLAGS) -o $@ $^ $(CFLAGSEND) $(GTKFLAGS)
 	@echo -e "=>\\e[92mCreating \\"\\e[4m$(@F)\\e[24m\\" into \\"\\e[4m$(@D)\\e[24m\\" Success\\e[0m"
 
 #create all object file (.o) for debug
 $(DBGBUILDDIR)src/%.o: %.c
 	@echo -e "=>\\e[94mPreparing \\"\\e[4m$<\\e[24m\\" to debug\\e[0m"
 	@mkdir -p $(DBGBUILDDIR)src/$(<D)
-	$(CC) $(CFLAGS) $(DBGCFLAGS) -c $< -o $@ $(GTKFLAGS)
+	$(CC) $(CFLAGS) $(DBGCFLAGS) -c $< -o $@ $(CFLAGSEND) $(GTKFLAGS)
 	@echo -e "=>\\e[92mBuilding \\"\\e[4m$(@F)\\e[24m\\" into \\"\\e[4m$(@D)\\e[24m\\" Success\\e[0m"
 
 clean:
